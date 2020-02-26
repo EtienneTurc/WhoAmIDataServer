61ea7398f53e13add891b35a3415bf74819c31e2
Christophe Boetto <christophe.boetto@ensta-paristech.fr>
2020-02-26 19:37:51 +0100

    added prod directory

--- Models/prod/main.py
+++ Models/prod/main.py
@@ -2,6 +2,8 @@ from flask import Flask, escape, request
 from flask_cors import CORS, cross_origin
 import time
 
+import pandas as pd
+
 app = Flask(__name__)
 CORS(app)
 
@@ -9,4 +11,10 @@ CORS(app)
 @app.route('/analytics', methods=['POST'])
 def simulate_long_request():
     print(request.json)
+
+    df = pd.read_json(request.json, encoding = 'utf-8')
+    df['cat'] = df.headers.apply(tag_mail)
+
+    
+
     return {"result":"non merci, bisous"}
