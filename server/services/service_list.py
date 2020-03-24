782f4f53d5c6bdf706cd28a2b24481d8650e2980
Paul <pazoulai@gmail.com>
2020-03-24 12:46:01 +0100

    clean

--- server/services/service_list.py
+++ server/services/service_list.py
@@ -4,7 +4,6 @@ from helpers.service import is_callback
 from helpers.redis_helpers import getData, setData
 from services.utils.extract_services import extract_services
 from utils import tag_mail
-# from services.utils.extractWords import extractWords
 
 
 @is_callback(["raw/google/mail"])
@@ -17,9 +16,6 @@ def google_mail_service(token):
     setData(token, "toDisplay.amazon.data", res["amazon"])
     setData(token, "toDisplay.amazon.meta.processing.google_mail", False)
     setData(token, "toDisplay.doctolib.data", res["doctolib"])
+    setData(token, "toDisplay.doctolib.meta.processing", False)
     setData(token, "toDisplay.lydia.data", res["lydia"])
     setData(token, "toDisplay.lydia.meta.processing.google_mail", False)
-    print("data saved to redis")
-    # except Exception as exception:
-    #     print(exception)
-    print("data was print")
