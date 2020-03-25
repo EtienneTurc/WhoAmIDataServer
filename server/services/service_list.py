a96a88a633082266fabea6b834bed086a3046a1d
Paul <pazoulai@gmail.com>
2020-03-25 12:12:36 +0100

    generating words from sent mails

--- server/services/service_list.py
+++ server/services/service_list.py
@@ -2,19 +2,18 @@ import pandas as pd
 
 from helpers.service import is_callback
 from helpers.redis_helpers import getData, setData
-from services.utils.extract_services import extract_services
-from services.utils.extract_words import getWords
-from utils import tag_mail
+from services.utils.extract import extract_services, tag_mail
+from services.utils.words import getWords
+# from utils import tag_mail
 
 
 @is_callback(["raw/google/mail"])
 def google_mail_service(token):
     print("beginning processing for raw/google/mail")
-    df = pd.DataFrame()
     try:
-        df = pd.DataFrame(getData(token, "raw.google.mail.received"))
-        df['cat'] = df.headers.apply(tag_mail)
-        res = extract_services(df)
+        df_received = pd.DataFrame(getData(token, "raw.google.mail.received"))
+        df_received['cat'] = df_received.headers.apply(tag_mail)
+        res = extract_services(df_received)
 
         setData(token, "toDisplay.amazon.data", res["amazon"])
         setData(token, "toDisplay.amazon.meta.processing.google_mail", False)
@@ -28,13 +27,16 @@ def google_mail_service(token):
         setData(token, "toDisplay.uberRides.data", res["uber_rides"])
         setData(token, "toDisplay.uberRides.meta.processing.google_mail", False)
 
-        setData(token, "toDisplay.uberBikes.data", res["uber_bicycle"])
+        setData(token, "toDisplay.uberBikes.data", res["uber_jump"])
         setData(token, "toDisplay.uberBikes.meta.processing.google_mail", False)
 
         setData(token, "toDisplay.uberEats.data", res["uber_eats"])
         setData(token, "toDisplay.uberEats.meta.processing.google_mail", False)
 
-        word_list = getWords(df)
+
+        df_sent = pd.DataFrame(getData(token, "raw.google.mail.sent"))
+
+        word_list = getWords(df_sent)
 
         setData(token, "toDisplay.words.data", word_list)
         setData(token, "toDisplay.words.meta.processing.google_mail", False)
